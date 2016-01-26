27664617953c975f7b8cd8306bf9e40fa6168d39
Johan <johaa1993@gmail.com>
2016-01-26 15:42:52 +0100

    test keys

---
+++ unit_testing_glfw/src/test_keys.adb
@@ -0,0 +1,35 @@
+with GLFW;
+with GLFW.Keys;
+with Interfaces.C;
+with Ada.Text_IO;
+
+
+procedure Test_Keys is
+
+   use GLFW;
+   use GLFW.Keys;
+   use Interfaces.C;
+   use Ada.Text_IO;
+
+   W : Window;
+   K : Action;
+
+begin
+
+   Initialize;
+
+   W := Create (400, 400, To_C ("Hello"));
+   Make_Context_Current (W);
+
+   loop
+      Wait_Events;
+
+      K := Get (W, Key_Enter);
+      Put_Line (K'Img);
+
+      pragma Warnings (Off);
+      exit when Closing (W) = 1;
+      pragma Warnings (On);
+   end loop;
+
+end;
